0738c75b5a509cdfc10a8c16183444afefd5c5dd
Laurent Peuch <cortex@worlddomination.be>
2014-02-24 04:30:34 +0100

    [mod] simplify exception, don't need all of those

--- rply/errors.py
+++ rply/errors.py
@@ -17,19 +17,7 @@ class LexingError(Exception):
         return self.source_pos
 
 
-class ParsingError(Exception):
-    """
-    Raised by a Parser, if no production rule can be applied.
-    """
-    def __init__(self, message, source_pos):
-        self.message = message
-        self.source_pos = source_pos
-
-    def getsourcepos(self):
-        """
-        Returns the position in the source, at which this error occurred.
-        """
-        return self.source_pos
+class ParsingError(Exception): pass
 
 
 class ParserGeneratorWarning(Warning):
