f25e416ab5adccda221f979ff87f15498640fd5c
Laurent Peuch <cortex@worlddomination.be>
2014-03-01 03:35:35 +0100

    [enh] add the posibility for a token to hide surrounding tokens

--- rply/token.py
+++ rply/token.py
@@ -18,12 +18,13 @@ class Token(BaseBox):
                        position of the first character in the source from which
                        this token was generated.
     """
-    def __init__(self, name, value, before_space="", after_space="", source_pos=None):
+    def __init__(self, name, value, before_space="", after_space="", source_pos=None, hidden_tokens=None):
         self.name = name
         self.value = value
         self.before_space = before_space
         self.after_space = after_space
         self.source_pos = source_pos
+        self.hidden_tokens = hidden_tokens if hidden_tokens else []
 
     def __repr__(self):
         return "Token(%r, %r)" % (self.name, self.value)
